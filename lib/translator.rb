194e0dc9cb3a617072e9dd3337444e108fcfe7d1
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-24 14:49:35 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,10 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  # code goes here
+  library = load_library(file_path)
+  
+  
+  
 end
 
 def get_english_meaning
