b860c8875f133cebf782ed333cecc6adad034814
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-24 14:51:27 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,9 +18,11 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   
-  
-  library["get_emoticon"][emoticon]
-  
+  if library["get_emoticon"].keys.include?(emoticon) == true
+    library["get_emoticon"][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
   
   
 end
